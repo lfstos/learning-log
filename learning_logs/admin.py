fabe15dea54dc7d921d667c5019d0a17bb86ea2b
Luiz Fernando <luiz-fernando@outlook.com>
2018-05-11 16:31:34 -0300

    Registrado o modelo Topic

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Topic
 
-# Modelo que o Django inclui automaticamente no site de administração 
+# O Django inclui alguns modelos no site de administração de modo automático,
+# por exemplo, User e Group, mas os modelos que criei devem ser registrados
+# manualmente.
 admin.site.register(Topic)
+# Esse código importa o modelo que queremos registrar, Topic, e então usa
+# admin.site.register() para dizer ao Django que administre nosso modelo por
+# meio do site de administração.
