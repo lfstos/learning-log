661ae5a5dcbb8c54765bd3eafb9bbf3c2c2b6d87
Luiz Fernando <luiz-fernando@outlook.com>
2018-05-11 19:46:23 -0300

    Registrando modelo Entry

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Topic
+from .models import Topic, Entry
 
 # O Django inclui alguns modelos no site de administração de modo automático,
 # por exemplo, User e Group, mas os modelos que criei devem ser registrados
 # manualmente.
 admin.site.register(Topic)
 # Esse código importa o modelo que queremos registrar, Topic, e então usa
+# admin.site.register() para dizer ao Django que administre nosso modelo por
+# meio do site de administração.
+
+admin.site.register(Entry)
+# Esse código importa o modelo que queremos registrar, Entry, e então usa
 # admin.site.register() para dizer ao Django que administre nosso modelo por
 # meio do site de administração.
