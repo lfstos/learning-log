2f38807b0663f036aeaa09ce24433af0868d304d
Luiz Fernando <luiz-fernando@outlook.com>
2018-05-12 11:21:04 -0300

    Criado a view index

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+# render renderiza a resposta de acordo com os dados fornecidos pelas views.
+
 # Create your views here.
+
+def index(request):
+    """A página inicial de learning_logs."""
+    # A função render utiliza dois argumentos, o objeto request original e um
+    # template que pode ser usado para construir a página.
+    return render(request, 'learning_logs/index.html')
