18e68f4518919024d983efbd6cc8c7778b062674
Luiz Fernando <luiz-fernando@outlook.com>
2018-05-14 18:37:40 -0300

    Adicionado path topic

---
+++ learning_logs/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+"""Define padrões de URL para learning_logs."""
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topic/<int:topic_id>/', views.topic, name='topic')
+]
