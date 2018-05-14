a47c638938e0431ee5850d1a35c57c3de61bd06a
Luiz Fernando <luiz-fernando@outlook.com>
2018-05-14 18:35:51 -0300

    Adicionado comentários

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
 # render renderiza a resposta de acordo com os dados fornecidos pelas views.
+
+from .models import Topic
+# Importando o modelo associado que precisamos
 
 # Create your views here.
 
@@ -9,3 +11,20 @@ def index(request):
     # A função render utiliza dois argumentos, o objeto request original e um
     # template que pode ser usado para construir a página.
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos."""
+    topics = Topic.objects.order_by('date_added')
+    # Consultamos o banco de dados pedindo os objetos Topic, ordenados de acprdo
+    # com o atributo date_added. Armazenamos o queryset resultante em topics.
+    return render(request, 'learning_logs/topics.html', {'topics': topics})
+
+def topic(request, topic_id):
+    """Mostra um assunto e todas as suas entradas."""
+    topic = Topic.objects.get(id=topic_id)
+    # Recuperamos as entradas associadas ao assunto.
+    # O sinal de menos na frente de date_added ordena os resultados em
+    # ordem inversa
+    entries = topic.entry_set.order_by('-date_added')
+    return render(request, 'learning_logs/topic.html', {'topic': topic,
+                                                        'entries': entries})
