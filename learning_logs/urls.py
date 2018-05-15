1659b22e654cf0c97f5aa5a67582d6ef634c8f90
Luiz Fernando <luiz-fernando@outlook.com>
2018-05-15 10:17:48 -0300

    Adicionado path para new topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,6 +8,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
-    path('topic/<int:topic_id>/', views.topic, name='topic')
-    path('new_topic', views.new_topic, name='new_topic')
+    path('topic/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic', views.new_topic, name='new_topic'),
 ]
