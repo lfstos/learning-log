3b4ba387ba2e818590806c15be72fe4dcc8bb379
Luiz Fernando <luiz-fernando@outlook.com>
2018-05-17 16:29:44 -0300

    Removido espaços

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,27 +1,32 @@
 from django import forms
+
+# Importa os Modelos Topic e Entry para criar automaticamente os formulários.
 from .models import Topic, Entry
 
+# Definimos uma classe chamada TopicForm que herda de forms.ModelForm
 class TopicForm(forms.ModelForm):
+    # A versão mais simples de um ModelForm é constituída de uma classe Meta
     class Meta:
+        # Diz ao Django em qual modelo o formulário deve se basear.
+        # Criamos o formulário a partir do modelo Topic.
         model = Topic
+        # Quais os campos devem ser incluídos nesse formulário.
         fields=['text']
         labels = {'text': ''}
-# Inicialmente importamos o módulo forms e o modelo Topic.
-# Definimos uma classe chamada TopicForm que herda de forms.ModelForm
-# A versão mais simples de um ModelForm é constituída de uma classe Meta
-# aninhada que diz ao Django em qual modelo o formulário deve se basear
-# e quais campos devem ser incluídos nesse formulário.
 
+# Definimos a classe chamada EntryForm que herda de forms.ModelForm
 class EntryForm(forms.ModelForm):
+    # A versão mais simples de um ModelForm é constituída de uma classe Meta
     class Meta:
+        # Diz ao Django em qual modelo o formulário deve se basear.
         model = Entry
+        # Quais os campos que serão inclusos nesses formulários
         fields = ['text']
+        labels={'text': ''}
+        # Um widget é um elemento de formulário HTML, por exemplo, uma caixa de texto de
+        # uma única linha, uma área de texto com várias linhas ou uma lista suspensa.
+        # Ao incluir o atirbuto widgets, podemos sobrescrever as opções default de
+        # widgets de Django. Ao usar um elemento forms.Textarea, estamos personalizando
+        # o widget de entrada para o campo 'text' de modo que a área de texto tenha 80
+        # colunas, em vez de usar o default de 40.
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
-# A classe herda de forms.ModelForm e tem uma classe Meta aninhada que lista o
-# modelo no qual ela está baseada e o campo a ser incluído no formulário.
-# Um widget é um elemento de formulário HTML, por exemplo, uma caixa de texto de
-# uma única linha, uma área de texto com várias linhas ou uma lista suspensa.
-# Ao incluir o atirbuto widgets, podemos sobrescrever as opções default de
-# widgets de Django. Ao usar um elemento forms.Textarea, estamos personalizando
-# o widget de entrada para o campo 'text' de modo que a área de texto tenha 80
-# colunas, em vez de usar o default de 40.
