886413c0358cbe87132562c529c6fb60f1fada76
Luiz Fernando <luiz-fernando@outlook.com>
2018-05-17 16:33:14 -0300

    criado URL edit_entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     path('new_topic', views.new_topic, name='new_topic'),
     path('topic/<int:topic_id>/', views.topic, name='topic'),
+    # Devemos adicionar um argumento topic_id no URL para adicionar uma nova
+    # entrada, pois ela deve estar associada a um assunto em particular.
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
